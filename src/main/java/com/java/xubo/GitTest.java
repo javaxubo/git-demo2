c7561111ef8bb5caef7ddf09aca9a1dbbb8042ff
xubo <xubo@java.com>
2023-01-09 22:08:35 +0800

    master commit2

--- src/main/java/com/java/xubo/GitTest.java
+++ src/main/java/com/java/xubo/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello git!!!");
         System.out.println("hello git2!!!");
         System.out.println("hello hot-fix test!!!");
+        System.out.println("master test 今天是周二");
+
     }
 }
